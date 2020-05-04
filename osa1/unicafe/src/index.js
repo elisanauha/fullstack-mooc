fcf0bf24b2170ccfad04e8fd4a3eee50dedfe304
Nauha Elisa <elisa.nauha@kela.fi>
2020-05-04 10:48:55 +0300

    Do unicafe step 3

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -31,6 +31,19 @@ const Positive = (props) => {
   )
 }
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <Stats text="good" stat={props.good}></Stats>
+      <Stats text="neutral" stat={props.neutral}></Stats>
+      <Stats text="bad" stat={props.bad}></Stats>
+      <Average good={props.good} neutral={props.neutral} bad={props.bad}></Average>
+      <Positive good={props.good} neutral={props.neutral} bad={props.bad}></Positive>
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -54,12 +67,8 @@ const App = () => {
       <Button name="good" handleClick={handleGood}></Button>
       <Button name="neutral" handleClick={handleNeutral}></Button>
       <Button name="bad" handleClick={handleBad}></Button>
-      <h1>Statistics</h1>
-      <Stats text="good" stat={good}></Stats>
-      <Stats text="neutral" stat={neutral}></Stats>
-      <Stats text="bad" stat={bad}></Stats>
-      <Average good={good} neutral={neutral} bad={bad}></Average>
-      <Positive good={good} neutral={neutral} bad={bad}></Positive>
+
+      <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
     </div>
   )
 }
