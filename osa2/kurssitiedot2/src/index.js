f0f202fbedca5a88d074acc2bf31857d12134426
Nauha Elisa <elisa.nauha@kela.fi>
2020-05-08 09:32:35 +0300

    Start osa2 kurssitiedot

---
+++ osa2/kurssitiedot2/src/index.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  const partsRendered = props.parts.map((part, index) => <Part part={part} key={index} ></Part>)
+
+  return (
+    <div>
+      {partsRendered}
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part.name} {props.part.exercises}</p>
+  )
+}
+
+const Total = (props) => {
+  let total = 0
+  props.parts.forEach(part => { total += part.exercises })
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name}></Header>
+      <Content parts={course.parts}></Content>
+      <Total parts={course.parts}></Total>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
