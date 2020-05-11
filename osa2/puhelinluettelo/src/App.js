3d3b81f953acb622b5fe68fb74bd632b764712eb
Nauha Elisa <elisa.nauha@kela.fi>
2020-05-11 11:41:02 +0300

    Do puhelinluettole step 3 add number

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,9 +2,13 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {
+      name: 'Arto Hellas',
+      number: '040-1231244'
+    }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -16,11 +20,13 @@ const App = () => {
       window.alert(`${newName} is already added to phonebook`)
     } else {
       const personObject = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
 
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     }
 
 
@@ -34,11 +40,14 @@ const App = () => {
           name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={(event) => setNewNumber(event.target.value)} />
+        </div>
+        <div>
           <button type="submit" onClick={addPerson}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <p key={person.name}>{person.name}</p>)}
+      {persons.map((person) => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 
