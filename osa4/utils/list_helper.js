db74d4eb878477ab312f772f9ef66ac339bbb475
elisanauha <elisa.nauha@gmail.com>
2020-06-01 14:36:48 +0300

    4.5 test favorite blog

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,6 +1,7 @@
 const dummy = (blogs) => {
   return 1
 }
+
 const totalLikes = (blogs) => {
   const reducer = (sum, item) => {
     return sum + item.likes
@@ -8,7 +9,22 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return {}
+  const reducer = (maxLikes, item) => {
+    return Math.max(maxLikes, item.likes)
+  }
+  const favorite = blogs.find((blog) => blog.likes === blogs.reduce(reducer, 0))
+  const strippedFavorite = {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  }
+  return strippedFavorite
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
