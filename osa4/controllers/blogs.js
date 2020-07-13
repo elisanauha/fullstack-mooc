43da6702a9503e5f3f84d9ef74964afa8962a250
elisanauha <elisa.nauha@gmail.com>
2020-07-13 13:54:36 +0300

    Do 4.17 reference user/blog and populate

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,23 +1,36 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const results = await Blog.find({})
+  const results = await Blog.find({}).populate('user', {
+    username: 1,
+    name: 1,
+    id: 1,
+  })
 
   response.json(results)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+
+  const user = await User.findById(body.userId)
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes === undefined ? 0 : body.likes,
+    user: user._id,
   })
 
-  const result = await blog.save()
-  return response.json(result.toJSON())
+  const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  return response.json(savedBlog.toJSON())
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
