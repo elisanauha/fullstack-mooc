69a7354ff9f7b2496bed980e9a58556b99f965eb
elisanauha <elisa.nauha@gmail.com>
2020-07-21 11:00:21 +0300

    Do 5.14-15 fire view and like buttons

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders content', () => {
+describe('<Blog />', () => {
+  let component
   const blog = {
     title: 'New try at post populate',
     author: 'Me myself and I',
@@ -17,14 +18,45 @@ test('renders content', () => {
     id: '5f158b61fd49f6700654176d',
   }
 
-  const component = render(
-    <Blog
-      blog={blog}
-      like={() => console.log('liked')}
-      remove={() => console.log('removed')}
-      usersPost={false}
-    />
-  )
+  const like = jest.fn()
 
-  expect(component.container).toHaveTextContent(`${blog.title} ${blog.author}`)
+  beforeEach(() => {
+    component = render(
+      <Blog
+        blog={blog}
+        like={like}
+        remove={() => console.log('removed')}
+        usersPost={false}
+      />
+    )
+  })
+
+  test('renders content', () => {
+    expect(component.container).toHaveTextContent(
+      `${blog.title} ${blog.author}`
+    )
+    expect(component.container).not.toHaveTextContent(`${blog.url}`)
+    expect(component.container).not.toHaveTextContent(`likes ${blog.likes}`)
+  })
+
+  test('renders extra content when wanted', () => {
+    const button = component.getByText('view')
+    fireEvent.click(button)
+
+    expect(component.container).toHaveTextContent(
+      `${blog.title} ${blog.author}`
+    )
+    expect(component.container).toHaveTextContent(`${blog.url}`)
+    expect(component.container).toHaveTextContent(`likes ${blog.likes}`)
+  })
+
+  test('pressing like twice calls it twice', () => {
+    const viewButton = component.getByText('view')
+    fireEvent.click(viewButton)
+    const likeButton = component.getByText('like')
+    fireEvent.click(likeButton)
+    fireEvent.click(likeButton)
+
+    expect(like.mock.calls).toHaveLength(2)
+  })
 })
